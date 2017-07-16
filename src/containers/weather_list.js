5ce6860869852b05802e4d0e9d97e8ab8e9c5399
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-16 10:01:49 +0700

    Make data more convenient to read

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import _ from 'lodash';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
-    const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="°C" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
@@ -24,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temparature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temparature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
