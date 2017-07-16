bddd55043ef2dd531236d4d2ca3d75be9bf12adb
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-16 09:37:33 +0700

    Succesful render temp as chart with reactsparkline

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log(temps);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={140} width={200} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
