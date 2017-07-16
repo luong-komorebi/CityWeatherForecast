94cfc77e62af8931b8cb4f303d7b78a38d059761
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-16 09:22:49 +0700

    Working with weatherlist
    - Build a list container
    - Mapping props to render helper
    - Fix url to get and api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_KEY='929c0b8071528648968b6848a3690abd';
-const ROOT_URL=`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = '5f679d8b18a5d886b5fb98cf4b0dd075';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-  
+
   return ({
     type: FETCH_WEATHER,
     payload: request
